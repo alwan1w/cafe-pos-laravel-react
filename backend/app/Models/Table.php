d86c497306998de110d1c7b6bb3e6d04aea9ebee
alwan1w <alwanfa49@gmail.com>
2026-05-31 15:50:28 +0700

    feat: add Table model

--- backend/app/Models/Table.php
+++ backend/app/Models/Table.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Table extends Model
 {
-    //
+    use HasFactory;
+
+    // Kolom yang diizinkan untuk diisi secara massal (Mass Assignment)
+    protected $fillable = [
+        'number',
+        'capacity',
+        'status'
+    ];
 }
